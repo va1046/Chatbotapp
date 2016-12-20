e01fa1f8c02834059f1ffa218287aad741906e58
vamshi krishna <vamshi.krishna.naik@gmail.com>
2016-12-21 03:01:50 +0530

    Fourth Commit
    Remove other models

--- app/src/main/java/com/example/vamshi/docsapp/Model/ChatMessage.java
+++ app/src/main/java/com/example/vamshi/docsapp/Model/ChatMessage.java
@@ -1,13 +1,19 @@
 package com.example.vamshi.docsapp.Model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by vamshi on 20-12-2016.
  */
 
-public class ChatMessage {
+public class ChatMessage implements Parcelable {
 
     public boolean left;
     public String Message;
+
+    public ChatMessage() {
+    }
 
     public ChatMessage(boolean left, String message) {
         this.left = left;
@@ -28,5 +34,36 @@ public class ChatMessage {
 
     public void setMessage(String message) {
         Message = message;
+    }
+
+    public static final Parcelable.Creator<ChatMessage> CREATOR = new Creator<ChatMessage>() {
+        public ChatMessage createFromParcel(Parcel source) {
+            ChatMessage mChat = new ChatMessage();
+            mChat.left = source.readByte() != 0;
+            mChat.Message = source.readString();
+
+            return mChat;
+        }
+
+        public ChatMessage[] newArray(int size) {
+            return new ChatMessage[size];
+        }
+    };
+
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeByte((byte) (left ? 1 : 0));
+        parcel.writeString(getMessage());
+    }
+
+    private void readFromParcel(Parcel in) {
+        left = in.readByte() != 0;
+        Message = in.readString();
     }
 }
